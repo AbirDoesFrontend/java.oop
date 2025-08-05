82dc751a236356b29b182cfa77b30489e6e9302f
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2025-08-06 03:08:00 +0600

    Java: Hello World

---
+++ helloworld/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("I love  Java!");
+
+        System.out.print("My Name is Abir!\n");
+
+        System.out.printf("Here is a number %d", 10);
+
+    }
+}
